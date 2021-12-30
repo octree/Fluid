451dacb7063729c051266a7d7579d60a591079b0
Octree <fouljz@gmail.com>
2021-12-30 13:06:15 +0800

    🧱 Add VStack

--- Sources/Fluid/Container/VStack.swift
+++ Sources/Fluid/Container/VStack.swift
@@ -24,4 +24,113 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import Foundation
+import UIKit
+import CoreGraphics
+
+private struct VStackLayout {
+    let alignment: HorizontalAlignment
+    let spacing: CGFloat
+    let context: LayoutContext
+    struct Result {
+        var size: CGSize
+        var children: [(CGRect, MeasuredNode)]
+
+        mutating func normalize(minX: CGFloat) {
+            children.indices.forEach {
+                children[$0].0.origin.x -= minX
+            }
+        }
+    }
+
+    private func deal(with nodes: [MeasuredNode]) -> Result {
+        let spacing = self.spacing * CGFloat(nodes.count - 1)
+        var result = Result(size: .zero, children: [])
+        result.size.height = spacing
+        var minX: CGFloat = 0
+        let firstRect = CGRect(origin: .zero, size: nodes.first!.size)
+        var maxX: CGFloat = firstRect.maxX
+        var y = firstRect.height + spacing
+        result.children = [(firstRect, nodes.first!)]
+        result.size.height += nodes.first!.size.height
+
+        for node in nodes.dropFirst() {
+            var rect = CGRect(origin: .init(x: 0, y: y), size: node.size)
+            rect.formAlign(to: firstRect, alignment: alignment)
+            result.children.append((rect, node))
+            minX = min(minX, rect.minX)
+            maxX = max(maxX, rect.maxX)
+            y += rect.height + spacing
+            result.size.height += rect.height
+        }
+        result.size.width = maxX - minX
+        result.normalize(minX: minX)
+        return result
+    }
+
+    func layout(node: MeasurableNode, context: LayoutContext) -> MeasuredNode {
+        let node = node.layout(using: context)
+        precondition(node.size.height != .infinity)
+        return node
+    }
+
+    func layout(_ children: [MeasurableNode]) -> Result {
+        guard children.count > 0 else { return .init(size: .zero, children: []) }
+        let proposedSize = context.proposedSize
+        var collection = children.map { layout(node: $0, context: context) }
+        let spacing = self.spacing * CGFloat(children.count - 1)
+        let resumed = collection.reduce(0) { $0 + $1.size.height } + spacing
+        if proposedSize.height >= resumed {
+            return deal(with: collection)
+        }
+        let shrinkIndices = collection.indices.filter { children[$0] is ShrinkableNode }
+        guard shrinkIndices.count > 0 else {
+            return deal(with: collection)
+        }
+        let beforeHeight = shrinkIndices.reduce(0) { $0 + collection[$1].size.height }
+        let left = max(proposedSize.height - resumed + beforeHeight, 0)
+        let shrinked = shrinkIndices.map { collection[$0].size.height }.shrinkTo(width: left)
+        zip(shrinkIndices, shrinked).forEach {
+            let context = LayoutContext(width: context.proposedSize.width, height: $1)
+            collection[$0] = layout(node: children[$0], context: context)
+        }
+        return deal(with: collection)
+    }
+}
+
+public struct VStack: MeasurableNode {
+    public let alignment: HorizontalAlignment
+    public let spacing: CGFloat
+    public let content: MeasurableCollection
+
+    init(alignment: HorizontalAlignment, spacing: CGFloat, content: Group) {
+        self.alignment = alignment
+        self.spacing = spacing
+        self.content = content
+    }
+
+    public var children: [MeasurableNode] {
+        content.children
+    }
+
+    public func layout(using layoutContext: LayoutContext) -> MeasuredNode {
+        let result = VStackLayout(alignment: alignment, spacing: spacing, context: layoutContext)
+            .layout(children)
+        let content = self.content.measuredCollection(result.children)
+        return MeasuredVStack(size: result.size, alignment: alignment, spacing: spacing, content: content)
+    }
+}
+
+private struct MeasuredVStack: MeasuredNode {
+    let size: CGSize
+    let alignment: HorizontalAlignment
+    let spacing: CGFloat
+    let content: MeasuredCollection
+
+    var positionedChildren: [(CGRect, MeasuredNode)] {
+        content.children
+    }
+
+    func render(in view: UIView, origin: CGPoint) {
+        content.render(in: view, origin: origin)
+    }
+}
